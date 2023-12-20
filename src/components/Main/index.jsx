dfae61090d50f1d70b6b193e3481e798a422222c
Jeffrey <matheusunitt@gmail.com>
2023-12-20 10:51:37 -0300

    Especifica height dos boxes no main

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,14 +5,14 @@ const Main = ({ operacaoSelecionada }) => {
         {operacaoSelecionada.nome}
       </h1>
       <div className="container flex flex-col p-4 gap-4 rounded-lg bg-[#C7C7C7]">
-        <div className="container flex flex-row w-full items-center rounded-lg p-6 text-6xl font-bold gap-2 bg-gray-600">
+        <div className="container flex flex-row h-[130px] w-full items-center rounded-lg p-6 text-6xl font-bold gap-2 bg-gray-600">
           <span className="text-white">45</span>
           <span className="text-yellow-400">
             {operacaoSelecionada?.simbolo}
           </span>
           <span className="text-white">72</span>
         </div>
-        <div className="container flex flex-row w-full items-center rounded-lg p-6 text-6xl font-bold gap-2 bg-gray-500">
+        <div className="container flex flex-row h-[130px] w-full items-center rounded-lg p-6 text-6xl font-bold gap-2 bg-gray-500">
           <span className="text-emerald-500">=</span>
           <input
             type="text"
