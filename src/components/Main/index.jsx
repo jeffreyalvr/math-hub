115bd15741877950ad6c33fd9b1bb5163f7b38d9
Jeffrey <matheusunitt@gmail.com>
2023-12-20 11:10:05 -0300

    Adiciona div para filtrar valores da operação

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -4,6 +4,38 @@ const Main = ({ operacaoSelecionada }) => {
       <h1 className="pb-4 text-3xl font-bold capitalize text-[#6B7280] border-b border-[#B7B7B7]">
         {operacaoSelecionada.nome}
       </h1>
+
+      <div className="container flex flex-row justify-center p-4 gap-8 rounded-lg bg-[#C7C7C7]">
+        <span
+          className="flex flex-row gap-3 items-center"
+          title="Determina a quantidade de valores a serem calculados"
+        >
+          quantidade de valores
+          <select className="p-2 rounded-lg">
+            <option value="2">2 valores</option>
+            <option value="3">3 valores</option>
+            <option value="4">4 valores</option>
+            <option value="5">5 valores</option>
+          </select>
+        </span>
+
+        <span
+          className="flex flex-row gap-3 items-center"
+          title="Determina o alcance máximo de cada número calculado"
+        >
+          alcance dos valores
+          <select className="p-2 rounded-lg">
+            <option value="25">até 25</option>
+            <option value="100">até 100</option>
+            <option value="250">até 250</option>
+            <option value="500">até 500</option>
+            <option value="1000">até 1000</option>
+            <option value="5000">até 5000</option>
+            <option value="9999">até 9999</option>
+          </select>
+        </span>
+      </div>
+
       <div className="container flex flex-col p-4 gap-4 rounded-lg bg-[#C7C7C7]">
         <div className="container flex flex-row h-[130px] w-full items-center rounded-lg p-6 text-6xl font-bold gap-2 bg-gray-600">
           <span className="text-white">45</span>
