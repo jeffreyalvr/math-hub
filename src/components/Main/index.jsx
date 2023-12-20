f8ac9afacedd7d202d190aba823bfdfaefc309a5
Jeffrey <matheusunitt@gmail.com>
2023-12-20 10:45:33 -0300

    Reorganiza design do main

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,21 +1,33 @@
 const Main = ({ operacaoSelecionada }) => {
   return (
-    <div className="container flex flex-col md:flex-row p-4 gap-4 rounded-lg bg-gray-200">
-      <div className="container flex flex-row w-full items-center md:w-fit rounded-lg p-6 text-6xl font-bold gap-2 bg-gray-600">
-        <span className="text-white">45</span>
-        <span className="text-yellow-400">{operacaoSelecionada?.simbolo}</span>
-        <span className="text-white">72</span>
+    <div className="flex flex-col gap-8 max-w-3xl m-auto">
+      <h1 className="pb-4 text-3xl font-bold capitalize text-[#6B7280] border-b border-[#B7B7B7]">
+        {operacaoSelecionada.nome}
+      </h1>
+      <div className="container flex flex-col p-4 gap-4 rounded-lg bg-[#C7C7C7]">
+        <div className="container flex flex-row w-full items-center rounded-lg p-6 text-6xl font-bold gap-2 bg-gray-600">
+          <span className="text-white">45</span>
+          <span className="text-yellow-400">
+            {operacaoSelecionada?.simbolo}
+          </span>
+          <span className="text-white">72</span>
+        </div>
+        <div className="container flex flex-row w-full items-center rounded-lg p-6 text-6xl font-bold gap-2 bg-gray-500">
+          <span className="text-emerald-500">=</span>
+          <input
+            type="text"
+            className="w-fit text-6xl bg-transparent border-0 outline-none text-cyan-400"
+          />
+        </div>
+        <div className="flex flex-row justify-center gap-4 my-4">
+          <button className="py-4 px-12 rounded-lg bg-[#4984CA] text-white font-bold hover:bg-opacity-75">
+            Verificar
+          </button>
+          <button className="py-4 px-12 rounded-lg bg-[#CA7849] text-white font-bold hover:bg-opacity-75">
+            Não sei
+          </button>
+        </div>
       </div>
-      <div className="container flex flex-row w-full items-center rounded-lg p-6 text-6xl font-bold gap-2 bg-gray-500">
-        <span className="text-emerald-500">=</span>
-        <input
-          type="text"
-          className="w-fit text-6xl bg-transparent border-0 outline-none text-cyan-400"
-        />
-      </div>
-      <button className="p-4 md:px-8 rounded-lg bg-blue-500 text-white font-bold hover:bg-opacity-75">
-        Verificar
-      </button>
     </div>
   );
 };
